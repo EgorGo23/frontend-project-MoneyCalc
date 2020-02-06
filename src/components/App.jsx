10dd2a05973f543b3cb9e35b9bfb2ed29f2d4180
Egor <egor.go2303>
2020-02-06 22:24:52 +0300

    ui selection

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,10 @@
 import React, { Component } from 'react';
-import '../../public/styles/App.scss';
+import { Input } from '@material-ui/core';
 
 class App extends Component {
-  // eslint-disable-next-line class-methods-use-this
   render() {
     return (
-      <div className='card'>
-        <div className="card-header">
-          Featured
-        </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">Cras justo odio</li>
-          <li className="list-group-item">Dapibus ac facilisis in</li>
-          <li className="list-group-item">Vestibulum at eros</li>
-        </ul>
-      </div>
+      <Input />
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
- ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
