1f6ab2a0c4883e34e1b5e506c3741b2447c9f1bb
EgorGo23 <egor.go2303@gmail.com>
2020-02-10 21:55:05 +0300

    layout creation. added field to select application

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Navbar from './Navbar.jsx';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Navbar />
-        <div className='container pt-4'>
-          <h1>Hello</h1>
+const App = () => (
+  <div className='container app'>
+    <div className="row align-items-center justify-content-center content">
+      <div className="col-7 col-content">
+        <div className="row btn-select">
+          <div className="col btn-todo"></div>
+          <div className="col btn-blq"></div>
         </div>
-      </>
-    );
-  }
-}
-
+        <div className="row top-panel">
+          <div className="col-8 left"></div>
+          <div className="col right"></div>
+        </div>
+        <div className="row main">
+          <div className="col-7 left"></div>
+          <div className="col right"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 export default App;
